3597e590e46cda5303da7795d0ad61e3500bcee7
Le0-dot <lkolyadich@gmail.com>
2024-02-02 16:49:58 +0100

    Update scripts to improve readability

--- .local/bin/clibboard_choose.sh
+++ .local/bin/clibboard_choose.sh
@@ -3,4 +3,10 @@
 
 # I need to replace tabs with spaces, since my tofi setup unable to display them
 
-cliphist list | sed 's/\t/    /' | tofi | cut -d' ' -f1 | xargs printf "%d\t" | cliphist decode | wl-copy
+cliphist list | 
+    sed 's/\t/    /' | 
+    tofi | 
+    cut -d' ' -f1 | 
+    xargs printf "%d\t" | 
+    cliphist decode | 
+    wl-copy
