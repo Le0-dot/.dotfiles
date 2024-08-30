c555d731c4e8a2d50e5a1e007de5c65b00d06226
Le0-dot <lkolyadich@gmail.com>
2024-08-30 18:15:49 +0200

    Remove old (X11) and unused configs

--- .config/qtile/config.py
+++
@@ -1,6 +0,0 @@
-from bar import extention_defaults, widget_defaults
-from groups import groups
-from keybindings import keys
-from layouts import layouts, mouse
-from screens import screens
-from settings import *
--- .config/qtile/screens.py
+++
@@ -1,7 +0,0 @@
-from bar import main_bar, secondary_bar
-from libqtile.config import Screen
-
-screens = [
-    Screen(top = main_bar),
-    Screen(top = secondary_bar), 
-]
