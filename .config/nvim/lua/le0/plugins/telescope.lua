f0b74d475420e6dea0b0893eeb2159ace0dee472
Le0-dot <lkolyadich@gmail.com>
2024-10-11 16:28:31 +0200

    Add missing keybing, removed while refactoring config

--- .config/nvim/lua/le0/plugins/telescope.lua
+++ .config/nvim/lua/le0/plugins/telescope.lua
@@ -18,7 +18,7 @@ return {
 		{ '<leader>ss',       builtin.builtin,                                   desc = '[S]earch [S]elect Telescope' },
 		{ '<leader>sw',       builtin.grep_string,                               desc = '[S]earch current [W]ord' },
 		{ '<leader>sg',       builtin.live_grep,                                 desc = '[S]earch by [G]rep' },
-		{ '<leader>sg',       '"zy:Telescope live_grep default_text=<C-r>z<CR>', desc = '[S]earch by [G]rep' },
+		{ '<leader>sg',       '"zy:Telescope live_grep default_text=<C-r>z<CR>', desc = '[S]earch by [G]rep', mode = 'v' },
 		{ '<leader>sd',       builtin.diagnostics,                               desc = '[S]earch [D]iagnostics' },
 		{ '<leader>sr',       builtin.resume,                                    desc = '[S]earch [R]esume' },
 		{ '<leader>s.',       builtin.oldfiles,                                  desc = '[S]earch Recent Files ("." for repeat)' },
