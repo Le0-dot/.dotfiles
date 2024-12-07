eacc5cd40ca6491cea47dcfc2e936cb4d70f3165
Le0-dot <lkolyadich@gmail.com>
2024-12-07 03:06:34 +0100

    Update nvim config

--- terminal/.config/nvim/lua/le0/plugins/mini.lua
+++ terminal/.config/nvim/lua/le0/plugins/mini.lua
@@ -2,12 +2,21 @@ return {
     'echasnovski/mini.nvim',
     config = function()
         require('mini.extra').setup()
-        require('mini.ai').setup()
         require('mini.surround').setup()
         require('mini.sessions').setup {
             autoread = true,
         }
-        require('mini.hipatterns').setup({
+
+        local ai = require('mini.ai')
+        ai.setup {
+            custom_textobjects = {
+                F = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+                c = ai.gen_spec.treesitter { a = '@conditional.outer', i = '@conditional.inner' },
+            }
+        }
+
+        local hipatterns = require('mini.hipatterns')
+        hipatterns.setup({
             highlighters = {
                 -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
                 fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
@@ -16,7 +25,7 @@ return {
                 note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
 
                 -- Highlight hex color strings (`#rrggbb`) using that color
-                hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
+                hex_color = hipatterns.gen_highlighter.hex_color(),
             },
         })
     end,
