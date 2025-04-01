0ee3903ed562f7c27e67b76a3cbad077f0f00538
Lev Koliadich <lkolyadich@gmail.com>
2025-04-01 16:33:52 +0200

    Replace mypy with ruff for default nvim python linter

--- terminal/.config/nvim/lua/le0/plugins/lint.lua
+++ terminal/.config/nvim/lua/le0/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			local lint = require('lint')
 			lint.linters_by_ft = {
-				python = { 'mypy' },
+				python = { 'ruff' },
 				markdown = { 'vale' },
 			}
 			local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
